5da147f4e52d151589683e5e88c1328f3c55a174
lecbych jakub <jakub.lecbych@avast.com>
2017-10-16 16:44:41 +0200

    init react

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import IntelligentSelectTreeInput from './IntelligentSelectTreeInput';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<IntelligentSelectTreeInput filterBy={"test2"}
+                                            displayvalue={"test"}
+                                            displayTermState={true}
+                                            displayParent={true}
+                                            compactMode={true}
+                                            Providers={[
+                                                {'type':"file", 'destination':'./source/test.json'},
+                                                {'type':"rawData", 'value':[{'tst':"tse"}, {'asdfsas':'dasfas'}]}
+                                            ]}
+
+
+    />,   document.getElementById('intelligent-select-tree-root'));
