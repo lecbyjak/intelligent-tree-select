a28e70d821e5048cdfc9a8a78dbf60c3c3d4d8af
jakub lecbych <lecbychjakub@gmail..com>
2018-07-16 10:48:25 +0200

    prepare for npm publish

--- source/tests/App.test.js
+++ source/tests/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../js/components/App';
+import {IntelligentTreeSelectComponent} from '../js/containers/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<IntelligentTreeSelectComponent />, div);
 });
