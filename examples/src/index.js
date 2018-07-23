3966a554775bb2f9371a0f90dacef0203d8fd1c5
jakub lecbych <lecbychjakub@gmail..com>
2018-07-23 12:31:28 +0200

    tmp

---
+++ examples/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import myJSON3 from '../../src/data/events.json'
+import './app.sass';
+import {IntelligentTreeSelect} from '../../src/components/containers/App';
+
+
+const provider1 = {
+  name: "provider1",
+  response: (searchInput) => new Promise((resolve) => setTimeout(resolve, 1500, myJSON3)),
+  simpleTreeData: true,
+  labelKey: "http://www.w3.org/2000/01/rdf-schema#label",
+  valueKey: "@id",
+  childrenKey: "subTerm",
+};
+
+ReactDOM.render(
+    <IntelligentTreeSelect simpleTreeData={true}
+                                    localOptions={myJSON3}
+                                    valueKey={"@id"}
+                                    labelKey={"http://www.w3.org/2000/01/rdf-schema#label"}
+      //labelValue={(labelKey) => labelKey[0]['@value']}
+                                    childrenKey={"subTerm"}
+
+                                    providers={[
+                                      provider1,
+                                    ]}
+    />,
+  document.getElementById('app')
+);
+
+
